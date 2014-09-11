1920d30967624da918885c557b5596fde55b07da
Michael Woeber <michaelwoeber83@aol.com>
2014-09-11 13:25:52 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcdermit::Application.config.secret_token = '438d7a5e25a15fef3d78b3e6f86a2e0658ce94a8ea3a49ea0e0f879a7b1313d389a705c6a3fcc5c084bdbec59d7fe27cd13cceaf678e5b3d7d26df54b788cadb'
